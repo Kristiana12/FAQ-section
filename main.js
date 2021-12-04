6d14046035863b069960715ee04c7c5ffc1cab85
Kristiana Leka <63458911+Kristiana12@users.noreply.github.com>
2021-12-04 08:50:48 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,14 +2,16 @@
 
 const questionBoxes = document.querySelectorAll('.question');
 
-const currentlyActiveClass = document.querySelectorAll('.question.active');
-
 //For Each box that is being clicked adds .active
 questionBoxes.forEach((question) => {
   question.addEventListener('click', () => {
-    //Removes the active class from the element that had it, before applying it to the new one
-    removeClass();
-    question.classList.add('active');
+    if (question.classList.contains('active')) {
+      question.classList.remove('active');
+    } else {
+      //Removes the active class from the element that had it, before applying it to the new one
+      removeClass();
+      question.classList.add('active');
+    }
   });
 });
 
